e45c88770d4408e938b974a8de225a586812517c
Matt Mills <mmills@2bn.net>
2022-04-22 20:13:58 -0600

    Comment out stats refresh until it works better, rename mod_data_load -> mod_metadata_ingest

--- run_periodic_ingest.sh
+++ run_periodic_ingest.sh
@@ -4,6 +4,7 @@ DATA_FOLDER='steam_workshop_data/'
 ARCHIVE_SUFFIX='archive_'
 cd /zpool0/share/stellar-mods/
 
+. /opt/esp/esp-idf/export.sh
 echo "Fetching data from Steam workshop"
 python3 steam_workshop.py
 
@@ -14,7 +15,7 @@ do
 	APPID_DIR=$(dirname $JUST_FOLDER)
 	APPID=$(basename $APPID_DIR)
 	echo "Periodic Ingest script ingesting data: AppID: $APPID, Date: $BASENAME"
-	python3 ./postgres_mod_data_load.py $APPID $JUST_FOLDER
+	python3 ./postgres_mod_metadata_ingest.py $APPID $JUST_FOLDER
 	echo "Zipping data: AppID: $APPID, Date: $BASENAME"
 	mkdir -p $ARCHIVE_SUFFIX$DATA_FOLDER$APPID/
 	zip -r -m -9 -o -D $ARCHIVE_SUFFIX$DATA_FOLDER$APPID/$BASENAME.zip $JUST_FOLDER
@@ -24,5 +25,5 @@ done
 python3 ./postgres_preview_download.py
 python3 ./postgres_mod_download.py
 python3 ./postgres_mod_filelist_load.py
-python3 ./postgres_mod_stats_refresh.py
+#python3 ./postgres_mod_stats_refresh.py
 
